045932d102f1a53796c35ac841a9a70ca25d2445
June youngOh <juneyoungoh@Juneui-MacBook-Pro.local>
2015-07-08 15:14:46 +0900

    add rand

--- scripts/jdroid.js
+++ scripts/jdroid.js
@@ -1,13 +1,22 @@
 //js version
 //2015 07 08 Juneyoung Oh
 
+
 module.exports = function(robot){
+
+	var fourword_replies = [
+		"Yeah, fuck you, too"
+		, "Suck my dick!"
+		, "Why? Call your mama"
+	];
+
 	robot.hear("I'm hugry", function(msg){
 		msg.send("Eat your own flesh");
 	}); 
 
 	robot.respond("fuck you", function(msg){
-		msg.reply("Yeah, fuck yo, too");
+		var pickIdx = Math.floor(Math.random() * fourword_replies.length)
+		msg.reply(fourword_replies[pickIdx]);
 	});
 
 	robot.hear("What is your name?", function(msg){

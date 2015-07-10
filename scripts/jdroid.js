e5b857de61af08a6a72d543c96848e93758bfecc
June youngOh <juneyoungoh@Juneui-MacBook-Pro.local>
2015-07-10 17:29:59 +0900

    aaa

--- scripts/jdroid.js
+++ scripts/jdroid.js
@@ -1,6 +1,7 @@
 //js version
 //2015 07 08 Juneyoung Oh
 
+//FortuneTeller
 
 module.exports = function(robot){
 
@@ -10,18 +11,14 @@ module.exports = function(robot){
 		, "Peter Piper picked a peck of pickled pepper. Did Peter Piper pick a peck of pickled pepper? If Peter Piper picked a peck of pickled pepper, Where's the peck of pickled pepper Peter Piper picked?"
 	];
 
-	robot.hear(/I'm hugry/igm, function(msg){
-		msg.send("So what?");
-	}); 
-
 	setInterval(60, function(){
 		robot.respond(function(msg){
 			var pickIdx = Math.floor(Math.random() * mumbling_replies.length)
-		msg.reply(mumbling_replies[pickIdx]);
+			msg.reply(mumbling_replies[pickIdx]);
 		});
 	});
 
-	robot.hear(/What is your name?/igm, function(msg){
+	robot.hear(/what is your name/igm, function(msg){
 		msg.send(robot.name);
 	});
 };
